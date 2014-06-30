e187bb439e2174162b7db05d2a5ed111036fd03f
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2014-06-30 16:52:12 -0400

    Added multiple properties and fixes

--- jquery-parallax.js
+++ jquery-parallax.js
@@ -3,6 +3,36 @@
 ;(function($) {
 
 	var elements = []
+	var windowH = 0
+	var windowW = 0
+
+	var getPropertyName = function(element) {
+		return ($(element).data('px-property-name') + '').split(';')
+	}
+
+	var getPropertyFormat = function(element) {
+		return ($(element).data('px-property-format') + '').split(';')
+	}
+
+	var getPropertyMinValue = function(element) {
+		return $.map(($(element).data('px-value-min') + '').split(';'), parseFloat)
+	}
+
+	var getPropertyMaxValue = function(element) {
+		return $.map(($(element).data('px-value-max') + '').split(';'), parseFloat)
+	}
+
+	var getEnterScrollPosition = function(element) {
+		return Math.max($(element).offset().top - $(window).height(), 0)  + (parseFloat($(element).data('enterOffset')) || 0)
+	}
+
+	var getLeaveScrollPosition = function(element) {
+		return Math.max($(element).offset().top + $(element).height(), 0) + (parseFloat($(element).data('leaveOffset')) || 0)
+	}
+
+	var getDepth = function(element) {
+		return parseFloat($(element).data('px-depth')) || 1
+	}
 
 	$.enableAllParallax = function() {
 		$('[data-px-property-name]').enableParallax()
@@ -14,38 +44,101 @@
 
 	$.fn.enableParallax = function() {
 		$(this).each(function(i, el) {
-			$(el).data('parallax_enabled', true).updateParallaxPosition()
+			$(el).data('px-enabled', true).updateParallaxPosition()
 		})
 	}
 
 	$.fn.disableParallax = function() {
 		$(this).each(function(i, el) {
-			$(el).data('parallax_enabled', false).resetParallaxPosition()
+			$(el).data('px-enabled', false).resetParallaxPosition()
 		})
 	}
 
 	$.fn.parallax = function(options) {
 
+		if (options === 'refresh') {
+
+			var animation = $(this).data('px-anim')
+			if (animation) {
+				animation.measure(animation, this)
+			}
+
+			return animation
+		}
+
 		if (options) {
+
+			var update = function(animation, element, progress, min, max) {
+				return progress * (max - min) + min
+			}
+
+			var refresh = function(animation, element) {
+
+				var depth = animation.depth
+				var enter = getEnterScrollPosition(element)
+				var leave = getLeaveScrollPosition(element)
+
+				animation.enter = enter
+				animation.leave = leave * depth
+
+				$(element).updateParallaxPosition()
+			}
+
+			var propertyName = []
+			if (typeof options.propertyName === 'string') {
+				propertyName = options.propertyName.split(';')
+			} else if (typeof options.propertyName === 'object' && options.propertyName) {
+				propertyName = options.propertyName
+			}
+
+			var propertyFormat = []
+			if (typeof options.propertyFormat === 'string') {
+				propertyFormat = options.propertyFormat.split(';')
+			} else if (typeof options.propertyFormat === 'object' && options.propertyFormat) {
+				propertyFormat = options.propertyFormat
+			}
+
+			var propertyMinValue = []
+			if (typeof options.propertyMinValue === 'string') {
+				propertyMinValue = $.map(options.propertyMinValue.split(';'), parseFloat)
+			} else if (typeof options.propertyMinValue === 'number') {
+				propertyMinValue = [options.propertyMinValue]
+			} else if (typeof options.propertyMinValue === 'object' && options.propertyMinValue) {
+				propertyMinValue = options.propertyMinValue
+			}
+
+			var propertyMaxValue = []
+			if (typeof options.propertyMaxValue === 'string') {
+				propertyMaxValue = $.map(options.propertyMaxValue.split(';'), parseFloat)
+			} else if (typeof options.propertyMaxValue === 'number') {
+				propertyMaxValue = [options.propertyMaxValue]
+			} else if (typeof options.propertyMaxValue === 'object' && options.propertyMaxValue) {
+				propertyMaxValue = options.propertyMaxValue
+			}
 
 			var animation = {
 				visible: false,
-				propertyName: options.propertyName || '',
-				propertyFormat: options.propertyFormat || '',
-				propertyMinValue: options.propertyMinValue || null,
-				propertyMaxValue: options.propertyMaxValue || null,
-				enter: getEnterScrollPosition(this),
-				leave: getLeaveScrollPosition(this)
+				propertyName: propertyName,
+				propertyFormat: propertyFormat,
+				propertyMinValue: propertyMinValue,
+				propertyMaxValue: propertyMaxValue,
+				enter: null,
+				leave: null,
+				depth: options.depth || 1,
+				update: options.update || update,
+				refresh: options.refresh || refresh
 			}
 
-			$(this).data('parallax_animation', animation).updateParallaxPosition()
+			animation.refresh(animation, this)
+
+			$(this).data('px-anim', animation)
 
 			elements.push(this)
 
 			return animation
 		}
 
-		return $(this).data('parallax_animation')
+		return $(this).data('px-anim')
 	}
 
 	$.fn.resetParallaxPosition = function() {
@@ -59,7 +152,7 @@
 
 	$.fn.updateParallaxPosition = function() {
 
-		if ($(this).data('parallax_enabled') === false)
+		if ($(this).data('px-enabled') === false)
 			return
 
 		var animation = $(this).parallax()
@@ -71,19 +164,12 @@
 			scroll <= animation.leave) {
 			if (animation.visible == false) {
 				animation.visible = true
+				console.log('Enter')
 			}
 		} else {
 			if (animation.visible) {
 				animation.visible = false
-			}
-		}
-
-		var min = animation.propertyMinValue
-		var max = animation.propertyMaxValue
-
-		if (animation.callback == null) {
-			animation.callback = function(progress) {
-				return progress * (max - min) + min
+				console.log('Leave')
 			}
 		}
 
@@ -96,84 +182,78 @@
 				if (progress > 1) progress = 1
 				if (progress < 0) progress = 0
 
-				$(this).css(animation.propertyName, (animation.propertyFormat || '%vpx').replace('%v', animation.callback(progress)))
+				var names = animation.propertyName
+				var formats = animation.propertyFormat
+				var mins = animation.propertyMinValue
+				var maxs = animation.propertyMaxValue
+
+				for (var i = 0; i < names.length; i++) {
+
+					var name = names[i]
+					var format = formats[i]
+					var min = mins[i]
+					var max = maxs[i]
+
+					var value = animation.update(animation, this, progress, min, max)
+
+					$(this).css(name, (format || '%vpx').replace('%v', value))
+				}
 			}
 		}
 	}
 
-	var getPropertyName = function(element) {
-		return $(element).data('px-property-name')
-	}
-
-	var getPropertyFormat = function(element) {
-		return $(element).data('px-property-format')
-	}
-
-	var getPropertyMinValue = function(element) {
-		return parseFloat($(element).data('px-value-min'), 10) || null
-	}
-
-	var getPropertyMaxValue = function(element) {
-		return parseFloat($(element).data('px-value-max'), 10) || null
-	}
-
-	var getEnterScrollPosition = function(element) {
-		return Math.max($(element).offset().top - $(window).height(), 0) + parseFloat($(element).data('enterOffset') || 0, 10)
-	}
-
-	var getLeaveScrollPosition = function(element) {
-		return Math.max($(element).offset().top + $(element).height(), 0) + parseFloat($(element).data('leaveOffset') || 0, 10)
-	}
-
 	$(document).ready(function() {
 
-		$('[data-px-property-name]').each(function(i, el) {
+		windowW = $(window).width()
+		windowH = $(window).height()
 
-			var animation = {
-				visible: false,
+		$('[data-px-property-name]').each(function(i, el) {
+			$(el).parallax({
 				propertyName: getPropertyName(el),
 				propertyFormat: getPropertyFormat(el),
 				propertyMinValue: getPropertyMinValue(el),
-				propertyMaxValue: getPropertyMaxValue(el),
-				enter: getEnterScrollPosition(el),
-				leave: getLeaveScrollPosition(el)
-			}
-
-			$(el).data('parallax_animation', animation).updateParallaxPosition()
-
-			elements.push(el)
+				propertyMaxValue: getPropertyMaxValue(el)
+			})
 		})
 
 		$('[data-px-depth]').each(function(i, el) {
 
-			var $el = $(el)
+			var offset = 0
 
-			var depth = parseFloat($el.data('px-depth'))
-
-			var min = 0
-			var max = $el.offset().top + $el.outerHeight()
-
-			var callback = function(progress) {
-				return progress * (max - min) + min
+			var update = function(animation, element, progress, min, max) {
+				return offset = ($(window).scrollTop() - animation.enter) - (((animation.leave - animation.enter) / animation.depth) * progress)
 			}
 
-			var animation = {
-				visible: false,
+			var refresh = function(animation, element) {
+
+				var depth = animation.depth
+				var enter = getEnterScrollPosition(element)
+				var leave = getLeaveScrollPosition(element)
+
+				animation.enter = enter - offset
+				animation.leave = enter + (leave - enter) * depth - offset
+				animation.height = $(element).outerHeight()
+
+				$(element).updateParallaxPosition()
+			}
+
+			$(el).parallax({
+				depth: getDepth(el),
 				propertyName: 'transform',
 				propertyFormat: 'translate3d(0, %vpx, 0)',
 				propertyMinValue: null,
 				propertyMaxValue: null,
-				enter: getEnterScrollPosition(el),
-				leave: getLeaveScrollPosition(el) * depth,
-				callback: callback
-			}
+				update: update,
+				refresh: refresh
+			})
 
-			$el.data('parallax_animation', animation).updateParallaxPosition()
-
-			elements.push(el)
+			console.log($(el).parallax())
 		})
 
 		$(window).on('resize', function(e) {
+
+			windowW = $(window).width()
+			windowH = $(window).height()
 
 			$.each(elements, function(i, el) {
 
@@ -183,9 +263,8 @@
 				if (animation == null)
 					return
 
-				animation.enter = getEnterScrollPosition(el)
-				animation.leave = getLeaveScrollPosition(el)
-				$el.updateParallaxPosition()
+				animation.refresh(animation, el)
+
 			})
 		})
 
